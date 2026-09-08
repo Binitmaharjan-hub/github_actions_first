812ed60fef4b3b8caf849b36a77b1592ffedf261
Binitmaharjan-hub <binitmaharjan94@gmail.com>
2026-09-08 19:41:46 +0545

    added

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-print("python with pull request actions")
-
+import requests
+ 
+def main():
+    r = requests.get("https://catfact.ninja/fact")
+    print("Cat Fact", r.json()["fact"])
+ 
+if __name__ == "__main__":
+    main()
